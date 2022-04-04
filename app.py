caa2928e6bd676609e1d18238d0603184da93754
aizathizami <97604188+aizathizami@users.noreply.github.com>
2022-04-04 21:26:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,38 @@
-import pandas as pd
 import streamlit as st
 
-
-st.title('Passing Mark Checker')
-
-st.sidebar.write("""
-A demo of passing mark checker algorithm
-""")
-
-st.sidebar.write ("For more info, please contact:")
-
-st.sidebar.write("<a href='https://www.linkedin.com/in/aizat-hizami-b57345b9/'>Aizat Hizami </a>", unsafe_allow_html=True)
+st.header("Mark Checking Demo")
 
 
-y = 50 # minimum passing score
+readme = st.checkbox("readme")
 
-while True:
-    x = input("Please enter your mark. To stop the algorithm, enter x .\n\n")
+if readme:
 
-    if x== "x":
-      print ("\nThank you for using our service.\n")
-      break
-      
-    else:
-        try:
-          val = int(x)
-          
-          if (val > 100 or val < 0):
-            print("\nPlease enter a valid mark.\n")
+    st.write("""
+        This is a mark checking demo. You may get the codes via [github](https://github.com/richieyuyongpoh/markchecking)
+        """)
+
+    st.write ("For more info, please contact:")
+
+    st.write("[Dr. Yong Poh Yu](https://www.linkedin.com/in/yong-poh-yu/)")
+    
+
+st.write("Please enter your mark. To stop the algorithm, enter x .\n\n"
+         
+mark = st.text_input('Enter the mark here', '50')
+         
+         
+try:
+    val = float(mark)
+            
+    if (val > 100 or val < 0):
+        st.write("\nPlease enter a valid mark.\n")
                 
     elif val >= y:
-                  print("\nYou passed your exam. Keep it up!\n")
-                
-                else:
-                  print("\nUnfortunately, you did not pass your exam. Work harder. You can make it.\n")
-                  
-        except ValueError:
-        print("\nPlease enter a number.\n")
+        st.write("\nYou passed your exam. Keep it up!\n")
+       
+    else:
+        st.write("\nUnfortunately, you did not pass your exam. Work harder. You can make it.\n")
+
+            
+except ValueError:
+    st.write("\nPlease enter a number.\n")
